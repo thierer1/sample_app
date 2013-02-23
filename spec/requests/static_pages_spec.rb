96ead3774fce9cb2a3df3b5243f54c2a542e563b
Chris Thierer <thierer1@umbc.edu>
2013-02-23 17:02:39 +0000

    Use let function in spec file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,6 +12,9 @@ require 'spec_helper'
 
 
 describe "Static pages" do 
+
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
@@ -19,7 +22,7 @@ describe "Static pages" do
 		end
 		it "should have the title 'Home'" do 
 			visit '/static_pages/home'
-			page.should have_selector('title', :text => ' | Home')
+			page.should have_selector('title', :text => "#{base_title} | Home")
 		end
 	end
 
@@ -30,7 +33,7 @@ describe "Static pages" do
 		end
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			page.should have_selector('title', :text => ' | Help')
+			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
 	end
 
@@ -41,7 +44,7 @@ describe "Static pages" do
 		end
 		it "should have the title 'About Us'" do 
 			visit '/static_pages/about'
-			page.should have_selector('title', :text => ' | About Us')
+			page.should have_selector('title', :text => "#{base_title} | About Us")
 		end 
 	end
 
@@ -52,7 +55,7 @@ describe "Static pages" do
 		end
 		it "should have the title 'Contact'" do 
 			visit '/static_pages/contact'
-			page.should have_selector('title', :text => ' | Contact')
+			page.should have_selector('title', :text => "#{base_title} | Contact")
 		end
 	end
 
