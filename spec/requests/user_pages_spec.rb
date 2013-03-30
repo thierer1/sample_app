2028a94c95452013c4fb30875fc8200010284444
Chris Thierer <thierer1@umbc.edu>
2013-03-30 15:30:40 +0000

    Replace missing password error message with user-friendly message

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,8 +32,7 @@ describe "User pages" do
         before { click_button submit } 
 
         it { should have_selector('title', text: 'Sign up') }
-        it { should have_content('The form contains 7 errors') }
-        it { should have_content('Password digest can\'t be blank') }
+        it { should have_content('The form contains 6 errors') }
         it { should have_content('Name can\'t be blank') }
         it { should have_content('Email can\'t be blank') }
         it { should have_content('Email is invalid') }
